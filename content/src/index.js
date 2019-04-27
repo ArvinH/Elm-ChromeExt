80fc9a370c5943c7a4baae1c2e98c9102e138f7d
黃首翰（ArvinH） <arvin0731@gmail.com>
2019-04-27 14:19:34 +0900

    feat: change the way we display

--- content/src/index.js
+++ content/src/index.js
@@ -1,31 +1,46 @@
 require('./index.css')
 
-const mountNode = document.createElement('div')
-document.body.prepend(mountNode)
+const mountNode = document.createElement('div');
+document.body.append(mountNode);
 
-import { Elm } from './Main.elm'
+import { Elm } from './Main.elm';
 
-let app
+let app;
 
 // document.addEventListener('click', () => {
 //   chrome.runtime.sendMessage({ kind: 'clicked' })
 // })
 
 document.addEventListener('mouseup', () => {
-  const selectedContent = window.getSelection().toString();
+  const selectedObj = window.getSelection();
+  const selectedNode = selectedObj.anchorNode;
+  const selectedParentEl = selectedNode.parentElement;
+  const selectedContent = selectedObj.toString();
+  if (!selectedContent) {
+    mountNode.style.display = 'none';
+    chrome.runtime.sendMessage({ kind: 'selected', selectedContent: '' });
+    return;
+  }
+  const selectedPosition = {
+    top: selectedParentEl.getBoundingClientRect().top,
+    left: selectedParentEl.getBoundingClientRect().left
+  };
+  mountNode.style.left = `${selectedPosition.left + window.scrollX}px`;
+  mountNode.style.top = `${selectedPosition.top + window.scrollY +40}px`;
+  mountNode.style.display = 'block';
   if (selectedContent && selectedContent!== '') {
-    chrome.runtime.sendMessage({ kind: 'selected', selectedContent })
+    chrome.runtime.sendMessage({ kind: 'selected', selectedContent });
   }
 });
 
-const port = chrome.runtime.connect({ name: 'broadcast' })
+const port = chrome.runtime.connect({ name: 'broadcast' });
 port.onMessage.addListener(state => {
   if (!app) {
     app = Elm.Main.init({
       node: mountNode,
       flags: state
-    })
-    return
+    });
+    return;
   }
-  app.ports.onState.send(state)
+  app.ports.onState.send(state);
 })
